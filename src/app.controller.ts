1711e8632a609a11a1e520adb9579ea09b0384cc
olegkrasilev <olegkrasilev@gmail.com>
2025-07-11 14:32:21 +0300

    feat: http expection filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @darraghor/nestjs-typed/controllers-should-supply-api-tags */
 /* eslint-disable @darraghor/nestjs-typed/api-method-should-specify-api-response */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,6 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
   getHello(): string {
+    throw new HttpException('123', 500);
     return this.appService.getHello();
   }
 }
